69f041e78c287968f78f87d91960920c5d49ee16
PersonPersonDiscord <44577517+PersonPersonDiscord@users.noreply.github.com>
2018-10-31 09:36:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
-const Express = require("express");
-const Http = require("http");
-const WebSocket = require("ws");
+const Net = require('net');
 
-const app = Express();
-const port = process.env.PORT;
+const server = Net.createServer();
 
-const httpServer = Http.createServer(app);
-const webSocketServer = new WebSocket.Server({server});
-
+var users = [];
 var names = ["Adams", "Baker", "Clark", "Davis", "Evans", "Frank", "Ghosh", "Hills", "Irwin", "Jones", "Klein", "Lopez", "Mason", "Nalty", "Ochoa", "Patel", "Quinn", "Reily","Smith","Trott","Usman","Valdo","White","Xiang", "Yakub","Zafar"];
-function random(array) {
-  return array[Math.floor(Math.random() * array.length)];
+
+function join() {
+    var name = users[Math.floor(Math.random() * users.length)];
+    if (users.indexOf(name) > -1)
+        return join();
+    
+    console.log(name + " Joined!");
+    return name;
 }
 
-webSocketServer.on("connection", (socket) => {
-  // Connected, can send initial message if wanted
-  var name = random(names);
-  
-  console.log(name + "Joined!");
-  
-  socket.on("message", (message) => {
-    console.log(
-    //connection is up, let's add a simple simple event
-    ws.on('message', (message: string) => {
-      console.log(name + " says: " + message);
+function leave(name) {
+    console.log(name + "Left!");
+    users.splice(users.indexOf(name), 1);
+}
+
+server.on("connection", (socket) => {
+    // Connected, can send initial message if wanted
+    var name = join();
+    
+    
+    socket.on("message", (message) => {
+        console.log(name + " says: " + message);
+    });
+    
+    socket.on("close", () => {
+        leave(name);
     });
     
     socket.send("Hello!");
 });
 
+const port = process.env.PORT;
 server.listen(port, () => {
     console.log(`Listening on port ${server.address().port}!`);
 });
